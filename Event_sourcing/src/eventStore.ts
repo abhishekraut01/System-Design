3e18e2e57858fc89f3874fe93c8f069efc84f4ed
Abhishek <Abhishekraut8697@gmail.com>
2025-10-12 23:00:59 +0530

    added event store

--- Event_sourcing/src/eventStore.ts
+++ Event_sourcing/src/eventStore.ts
@@ -1,1 +1,13 @@
-    
+import type { Event } from "./events.js";
+
+export class EventStore {
+    private events: Event[] = [];
+
+    addEvent(event: Event) {
+        this.events.push(event);
+    }
+
+    getEvents(accountId: string): Event[] {
+        return this.events.filter(e => 'accountId' in e.data && e.data.accountId === accountId);
+    }
+}
--- Event_sourcing/src/events.ts
+++ Event_sourcing/src/events.ts
@@ -1,4 +1,4 @@
 export type Event =
-  | { type: 'AccountCreated'; data: { accountId: string } }
-  | { type: 'MoneyDeposited'; data: { accountId: string; amount: number } }
-  | { type: 'MoneyWithdrawn'; data: { accountId: string; amount: number } };
+    | { type: 'AccountCreated'; data: { accountId: string } }
+    | { type: 'MoneyDeposited'; data: { accountId: string; amount: number } }
+    | { type: 'MoneyWithdrawn'; data: { accountId: string; amount: number } };

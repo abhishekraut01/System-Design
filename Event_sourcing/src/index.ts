cec3696114139a1e683dd4b1a1ad6798ec9a0c9f
Abhishek <Abhishekraut8697@gmail.com>
2025-10-12 23:02:25 +0530

    test

---
+++ Event_sourcing/src/index.ts
@@ -0,0 +1,21 @@
+import { EventStore } from "./eventStore";
+import { BankAccount } from "./bankAccount";
+
+const store = new EventStore();
+
+// Create new account
+const acc = BankAccount.createAccount(store, "A1");
+
+// Perform actions
+acc.deposit(1000);
+acc.withdraw(200);
+acc.deposit(500);
+
+console.log("Balance:", acc.getBalance()); // 1300
+
+// Rebuild account from scratch
+const rebuilt = new BankAccount("A1", store);
+console.log("Rebuilt balance:", rebuilt.getBalance()); // 1300 again
+
+console.log("\nAll Events:");
+console.log(store);
